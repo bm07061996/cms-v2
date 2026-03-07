8963574dcf598d21e34834b2fac67c6a28357d39
bm07061996 <89469130+bm07061996@users.noreply.github.com>
2026-03-07 19:01:39 +0530

    Modify MongoDB connection configuration
    Updated MongoDB connection settings to use DSN and changed database name.

--- config/database.php
+++ config/database.php
@@ -33,14 +33,8 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options' => [
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
-            ],
+            'dsn' => env('DB_MONGO_URI'),
+            'database' => env('DB_DATABASE', 'prcperumal'),
         ],
 
         'sqlite' => [
